080c59745cbc4b6a2b90c47bed5f0e19adbad02d
Yujing Zheng <yujing@inceptioglobal.ai>
2020-10-30 18:58:55 -0700

    init version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function getCurrentTime() {
+  return (performance.now() + performance.timeOrigin).toFixed(3);
+}
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentTime(getCurrentTime());
+    }, 0);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="current-time-box">{currentTime}</div>
     </div>
   );
 }
