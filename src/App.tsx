15bf6b6496fd038c038c2a02152929105723b6aa
Yujing Zheng <yujing@inceptioglobal.ai>
2020-10-30 20:23:12 -0700

    add pause button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { usePrevious } from "./utils/hooks/use-previous";
 import "./App.css";
-
-function getCurrentTime() {
-  return (performance.now() + performance.timeOrigin).toFixed(3);
-}
 
 function App() {
   const [currentTime, setCurrentTime] = useState(getCurrentTime());
+  const [paused, setPaused] = useState(false);
+  const lastTime = usePrevious(currentTime);
+
+  function getCurrentTime() {
+    return (performance.now() + performance.timeOrigin).toFixed(3);
+  }
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setCurrentTime(getCurrentTime());
-    }, 0);
+    let timer: NodeJS.Timeout;
+
+    if (!paused) {
+      timer = setTimeout(() => {
+        setCurrentTime(getCurrentTime());
+      }, 10);
+    }
     return () => {
-      clearTimeout(timer);
+      if (timer) {
+        clearTimeout(timer);
+      }
     };
   });
 
   return (
     <div className="App">
-      <div className="current-time-box">{currentTime}</div>
+      <div className="current-time-box">{paused ? lastTime : currentTime}</div>
+      <button
+        className="unix-button"
+        onClick={() => {
+          setPaused(!paused);
+        }}
+      >
+        {paused ? "Resume" : "Pause"}
+      </button>
     </div>
   );
 }
