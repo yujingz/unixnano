86e2c51247a520b8baadd13e657208f9a4c5b42f
Yujing Zheng <yujing@coscene.io>
2024-01-10 14:57:04 +0800

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import './App.css';
 import CountDown from './components/CountDown';
 import CurrentTime from './components/CurrentTime';
+import './styles/App.css';
 
 export default function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import './styles/index.css';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -10,11 +10,5 @@ import reportWebVitals from "./reportWebVitals";
 //   document.getElementById('root')
 // );
 
-const rootElement =
-  document.getElementById("root") || document.createElement("div");
+const rootElement = document.getElementById('root') || document.createElement('div');
 ReactDOM.createRoot(rootElement).render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
